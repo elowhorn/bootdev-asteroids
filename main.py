666a6f3322f354532351dc1d43c460f9f03a3178
elowhorn <evanwlowhorn@gmail.com>
2025-12-09 21:58:32 -0500

    added clock, tick and delta time

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -21,6 +23,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
